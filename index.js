01420e1fbc93be94dc28512016c0c00e5b0b7425
kongukjae <kongukjae@gmail.com>
2024-11-18 18:16:24 +0900

    kongukjae/issue3

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-window.addEventListener("DOMContentLoaded", function() {
-// 모든 DOM이 로드된 후 실행, dom === HTML, CSS
+window.addEventListener("DOMContentLoaded", function () {
+  // 모든 DOM이 로드된 후 실행, dom === HTML, CSS
 
-console.log('hello! github ci!');
+  console.log("hello! github ci!");
 
+  const root = document.getElementById("root");
+  console.log('root :', root);
 
 
 });
